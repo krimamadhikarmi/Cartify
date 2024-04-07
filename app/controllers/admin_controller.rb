2ec6b0dda7c7bc61adf89a0e6c7b44c5df957483
krimamadhikarmi <madhikrima20@gmail.com>
2024-04-08 05:17:08 +0545

    sign out and dashboard

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -2,6 +2,6 @@ class AdminController < ApplicationController
     before_action :authenticate_admin!
 
     def index
-        
+        @orders = Order.where(fullfilled: false).order(created_at: :desc).take(5)
     end
 end
