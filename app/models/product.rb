33af43f6c3c783c4df6d7a610be740b60e3c35e6
krimamadhikarmi <madhikrima20@gmail.com>
2024-04-08 09:29:30 +0545

    user layout category cart

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,7 +2,7 @@ class Product < ApplicationRecord
     has_one_attached :image
     belongs_to :category
 
-    has_many :stocks
+    has_many :stocks, dependent: :destroy
     has_many :order_products
 end
   
