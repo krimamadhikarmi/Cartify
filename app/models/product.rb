944105a848f79f58ca80641a24a2f867786bce4f
krimamadhikarmi <madhikrima20@gmail.com>
2024-04-08 15:21:08 +0545

    ass sku to product

--- app/models/product.rb
+++ app/models/product.rb
@@ -4,5 +4,17 @@ class Product < ApplicationRecord
 
     has_many :stocks, dependent: :destroy
     has_many :order_products
+
+    before_create :generate_sku
+    
+    private
+  
+    def generate_sku
+      loop do
+        self.sku = SecureRandom.hex(5).upcase # Generating a random 5-character hexadecimal string
+        break unless Product.exists?(sku: sku)
+      end
+    end
 end
   
+
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_04_07_223846) do
+ActiveRecord::Schema[7.1].define(version: 2024_04_08_083505) do
   create_table "active_storage_attachments", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -97,7 +97,9 @@ ActiveRecord::Schema[7.1].define(version: 2024_04_07_223846) do
     t.boolean "active"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "sku"
     t.index ["category_id"], name: "index_products_on_category_id"
+    t.index ["sku"], name: "index_products_on_sku", unique: true
   end
 
   create_table "stocks", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
